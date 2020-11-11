93576457f220c46c1100ae75a1c7cda74922296f
Bartłomiej Talarczyk <btalarczyk1@gmail.com>
2020-11-11 11:17:13 +0100

    constructor tests passed

--- PudelkoLibrary/Pudelko.cs
+++ PudelkoLibrary/Pudelko.cs
@@ -12,19 +12,62 @@ namespace PudelkoLibrary
         public double B;
         public double C;
 
-        public Pudelko(double a = 0, double b = 0, double c = 0, UnitOfMeasure unit = UnitOfMeasure.milimeter)
+        public Pudelko(double? a = null, double? b = null, double? c = null, UnitOfMeasure unit = UnitOfMeasure.meter)
         {
-            A = a;
-            B = b;
-            C = c;
+            double defaultVal = 0.1;
+            switch (unit)
+            {
+                case UnitOfMeasure.milimeter:
+                    defaultVal = 100;
+                    break;
+                case UnitOfMeasure.centimeter:
+                    defaultVal = 10;
+                    break;
+            }
+            A = a ?? defaultVal;
+            B = b ?? defaultVal;
+            C = c ?? defaultVal;
             this.unit = unit;
+            ConvertToMeters();
+            ValidateFields();
         }
+
+        public double Objetosc { get => A * B * C; }
+        public double Pole { get => 2 * A * B + 2 * A * C + 2 * B * C; }
 
         public static explicit operator Pudelko(double[] p) => new Pudelko(p[0], p[1], p[2]);
         public static explicit operator double[](Pudelko p) => new double[] { p.A, p.B, p.C };
         public static implicit operator ValueTuple<double, double, double>(Pudelko p) => (p.A, p.B, p.C);
         public static implicit operator Pudelko(ValueTuple<double, double, double> p) =>
             new Pudelko(p.Item1, p.Item2, p.Item3);
+
+        public override string ToString()
+        {
+            return ToString("A B C");
+        }
+
+        public string ToString(string format)
+        {
+            return ToString(format, CultureInfo.CurrentCulture);
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            string measure = unit.ToString();
+            return format.Replace("A", A.ToString() + measure)
+                .Replace("B", B.ToString() + measure)
+                .Replace("C", C.ToString() + measure);
+        }
+
+        public bool Equals(Pudelko other)
+        {
+            return A == other.A && B == other.B && C == other.C && unit == other.unit;
+        }
+
+        public IEnumerator<double> GetEnumerator()
+        {
+            return new PudelkoEnumerator(this);
+        }
 
         public double this[int index]
         {
@@ -61,38 +104,44 @@ namespace PudelkoLibrary
             }
         }
 
-        public override string ToString()
-        {
-            return ToString("A B C");
-        }
-
-        public string ToString(string format)
-        {
-            return ToString(format, CultureInfo.CurrentCulture);
-        }
-
-        public string ToString(string format, IFormatProvider formatProvider)
-        {
-            string measure = unit.ToString();
-            return format.Replace("A", A.ToString() + measure)
-                .Replace("B", B.ToString() + measure)
-                .Replace("C", C.ToString() + measure);
-        }
-
-        public bool Equals(Pudelko other)
-        {
-            return A == other.A && B == other.B && C == other.C && unit == other.unit;
-        }
-
-        public IEnumerator<double> GetEnumerator()
-        {
-            return new PudelkoEnumerator(this);
-        }
-
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
         }
+
+        private void ValidateFields()
+        {
+            if (A <= 0 || B <= 0 || C <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Dimensions of Pudelko can't be less than 0!");
+            }
+            if (unit == UnitOfMeasure.meter && (A >= 10 || B >= 10 || C >= 10))
+            {
+                throw new ArgumentOutOfRangeException("Pudelko can't be larger than 10 meters!");
+            }
+        }
+
+        private const int PRECISION = 3;
+
+        private void ConvertToMeters()
+        {
+            int divideBy=1;
+            switch (unit)
+            {
+                case UnitOfMeasure.milimeter:
+                    divideBy = 1000;
+                    break;
+                case UnitOfMeasure.centimeter:
+                    divideBy = 100;
+                    break;
+            }
+            A = FixPrecision(A / divideBy, PRECISION);
+            B = FixPrecision(B / divideBy, PRECISION);
+            C = FixPrecision(C / divideBy, PRECISION);
+            unit = UnitOfMeasure.meter;
+        }
+
+        private double FixPrecision(double d, int precision) => Math.Floor(d * Math.Pow(10, precision)) / Math.Pow(10, precision);
     }
 
     public class PudelkoEnumerator : IEnumerator<double>
