0cc4b275617dbfea20dd4a37ac5febaa9f4fd073
Bartłomiej Talarczyk <btalarczyk1@gmail.com>
2020-11-11 20:45:03 +0100

    kompresuj & sort Pudelko class

---
+++ PudelkoConsoleApp/Extensions/PudelkoExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using PudelkoLibrary;
+namespace cs_lab03_Pudelko.Extensions
+{
+    public static class PudelkoExtensionMethods
+    {
+        public static Pudelko Kompresuj(this Pudelko p)
+        {
+            double size = Math.Cbrt(p.Objetosc);
+            return new Pudelko(size, size, size);
+        }
+    }
+}
--- PudelkoConsoleApp/Program.cs
+++ PudelkoConsoleApp/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using PudelkoLibrary;
+using PudelkoLibrary.Enums;
+using cs_lab03_Pudelko.Extensions;
 
 namespace cs_lab03_Pudelko
 {
@@ -6,7 +10,33 @@ namespace cs_lab03_Pudelko
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Pudelko> pudelka = new List<Pudelko>();
+            pudelka.AddRange(new Pudelko[]{
+                new Pudelko(6.03, 2, 2.012),
+                new Pudelko(6.03, 2, 2.012).Kompresuj(),
+                new Pudelko(1, 1.5, 5),
+                new Pudelko(3, 2.5, 1),
+                new Pudelko(312.2, 2.5, 101.2, UnitOfMeasure.centimeter),
+                new Pudelko(312, 25, 1705, UnitOfMeasure.milimeter)
+            });
+            pudelka.Sort(SortPudelko);
+            foreach (Pudelko p in pudelka)
+            {
+                Console.WriteLine(p.ToString());
+            }
+        }
+
+        private static int SortPudelko(Pudelko p1, Pudelko p2)
+        {
+            if (p1.Objetosc == p2.Objetosc)
+            {
+                if (p1.Pole == p2.Pole)
+                {
+                    return (int)((p1.A + p1.B + p1.C) - (p2.A + p2.B + p2.C)*1000);
+                }
+                return p1.Pole < p2.Pole ? -1 : 1;
+            }
+            return p1.Objetosc < p2.Objetosc ? -1 : 1;
         }
     }
 }
